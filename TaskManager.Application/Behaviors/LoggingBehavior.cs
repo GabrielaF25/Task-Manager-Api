de1a1bea46e656b86ff7b651960f967839b3982f
Georgiana Luciu <Georgiana.Luciu@endava.com>
2026-06-29 15:53:11 +0300

    Created LoggingBehavior

---
+++ TaskManager.Application/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System.Buffers;
+using TaskManager.Application.Common.ResultPattern;
+
+namespace TaskManager.Application.Behaviors;
+
+public class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull where TResponse : Result
+{
+    private readonly ILogger<LoggingBehavior<TRequest, TResponse>> _logger;
+
+    public LoggingBehavior(ILogger<LoggingBehavior<TRequest, TResponse>> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        var requestName = typeof(TRequest).Name;
+        _logger.LogInformation("Handling {RequestName}", requestName);
+
+        var response = await next(cancellationToken);
+
+        if (response.IsSuccess)
+        {
+            _logger.LogInformation("Handled {RequestName} successfully", requestName);
+        }
+        else
+        {
+            _logger.LogWarning(
+                "Handled {RequestName} with errors: {Errors}",
+                requestName,
+                response.Errors);
+        }
+
+        return response;
+    }
+}
--- TaskManager.Application/DependencyInjection/DependencyInjection.cs
+++ TaskManager.Application/DependencyInjection/DependencyInjection.cs
@@ -28,6 +28,7 @@ public static class ServiceCollection
         services.AddAutoMapper(cfg => cfg.AddProfile<UserProfile>());
 
         //behaviors
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
         services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
         services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnitOfWorkBehavior<,>));
 
